02c568bce2d8c24d53b1ff97506ac2fe996cf894
Hellocrazy <jeongwook3935@gmail.com>
2022-07-12 17:07:14 +0900

    jjaavvaa

---
+++ Begin/src/day08/Yuzu.java
@@ -0,0 +1,26 @@
+package day08;
+
+public class Yuzu {
+	
+	private int yuzu;
+	private int sugar;
+	
+	
+	//setter----
+	public void setYuzu(int yuzu) {
+		this.yuzu = yuzu;
+	}
+	
+	public void setSugar(int sugar) {
+		this.sugar = sugar;
+	}
+	
+	//getter-----
+	public int getYuzu() {
+		return yuzu;
+	}
+	public int getSugar() {
+		return sugar;
+	}
+	
+}
